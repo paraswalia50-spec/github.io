53fdfafbabe3b41c52d1c02f63cc377ba61baae0
paraswalia50-spec <paraswalia50@gmail.com>
2026-04-15 11:27:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let text = "GIS | Remote Sensing | Geoinformatics";
+let i = 0;
+
+function typing() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typing, 50);
+  }
+}
+
+typing();
